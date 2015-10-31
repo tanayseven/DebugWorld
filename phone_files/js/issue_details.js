39b5657cc8d672a1d31ec7a02dafb90adcf677e1
Max PRUDHOMME <max.prudhomme@gmail.com>
2015-11-01 03:20:52 +0530

    Display list of issues
    Button problem solved added

--- phone_files/js/issue_details.js
+++ phone_files/js/issue_details.js
@@ -24,45 +24,63 @@ function CallRestService(request) {
     document.body.appendChild(script);
 }
 
-/*
-//getJSON('http://localhost/issue1.json').then(function(json) {
-	var data = json.result;
+function getQueryParams(qs) {
+    qs = qs.split('+').join(' ');
+
+    var params = {},
+        tokens,
+        re = /[?&]?([^=]+)=([^&]*)/g;
+
+    while (tokens = re.exec(qs)) {
+        params[decodeURIComponent(tokens[1])] = decodeURIComponent(tokens[2]);
+    }
+
+    return params;
+}
+
+var query = getQueryParams(document.location.search);
+
+//*
+getJSON('http://debugworld.herokuapp.com/fetch_issue?id=' + query.id).then(function(json) {
+	var data = json[0];
+	var html = "";
+
+	document.getElementById("issueTitle").innerText = data.name;
+	document.getElementById("creatorUsername").innerText = data.creator_username;
+	document.getElementById("issueDateCreated").innerText = data.dateCreated;
+	document.getElementById("issueDescription").innerText = data.description;
+	document.getElementById("issueCity").innerText = data.location.city;
+	document.getElementById("issueCountry").innerText = data.location.country;
+	document.getElementById("issueVotesUp").innerText = data.votes.up;
+	document.getElementById("issueVotesDown").innerText = data.votes.down;
+
+	html = "";
+	for (var i = 0; i < data.tags.length; ++i) {
+		if (i != 0)
+			html += ", ";
+		html += "<a href=\"#" + data.tags[i].id + "\">" + data.tags[i].value + "</a>";
+	}
+	document.getElementById("issueTags").innerHTML = html;
+
+	html = "";
+	for (var i = 0; i < data.comments.length; ++i) {
+		html += "<article class=\"uk-comment uk-panel-box " + (data.comments[i].creator_id == data.creator_id ? "uk-panel-box-primary" : "") + " uk-margin-bottom\"> \
+					<header class=\"uk-comment-header\"> \
+						<h4 class=\"uk-comment-title\">" + data.comments[i].creator_username + "</h4> \
+						<div class=\"uk-comment-meta\">" + data.comments[i].dateCreated + " | #" + data.comments[i].id + "</div> \
+					</header> \
+					<div class=\"uk-comment-body\">" + data.comments[i].comment + "</div> \
+				</article>";
+	}
+	document.getElementById("issueComments").innerHTML = html;
+
+	showMap(data.location.latitude, data.location.longitude);
 }, function(status) {
 	alert('Something went wrong.');
 });
-*/
+//*/
 
-var html = "";
 
-document.getElementById("issueId").innerText = data.id;
-document.getElementById("issueTitle").innerText = data.name;
-document.getElementById("creatorUsername").innerText = data.creator_username;
-document.getElementById("issueDateCreated").innerText = data.dateCreated;
-document.getElementById("issueDescription").innerText = data.description;
-document.getElementById("issueCity").innerText = data.location.city;
-document.getElementById("issueCountry").innerText = data.location.country;
-document.getElementById("issueVotesUp").innerText = data.votes.up;
-document.getElementById("issueVotesDown").innerText = data.votes.down;
-
-html = "";
-for (var i = 0; i < data.tags.length; ++i) {
-	if (i != 0)
-		html += ", ";
-	html += "<a href=\"#" + data.tags[i].id + "\">" + data.tags[i].value + "</a>";
-}
-document.getElementById("issueTags").innerHTML = html;
-
-html = "";
-for (var i = 0; i < data.comments.length; ++i) {
-	html += "<article class=\"uk-comment uk-panel-box " + (data.comments[i].creator_id == data.creator_id ? "uk-panel-box-primary" : "") + " uk-margin-bottom\"> \
-				<header class=\"uk-comment-header\"> \
-					<h4 class=\"uk-comment-title\">" + data.comments[i].creator_username + "</h4> \
-					<div class=\"uk-comment-meta\">" + data.comments[i].dateCreated + " | #" + data.comments[i].id + "</div> \
-				</header> \
-				<div class=\"uk-comment-body\">" + data.comments[i].comment + "</div> \
-			</article>";
-}
-document.getElementById("issueComments").innerHTML = html;
 
 /*
  * Maps
@@ -83,9 +101,8 @@ var showMap = function(latitude, longitude) {
 	map.entities.push(pin);
 }
 
-showMap(data.location.latitude, data.location.longitude);
 
-
+/*
 var getCityAndCountry = function(latitude, longitude, a) {
 	var url = "http://dev.virtualearth.net/REST/v1/Locations/" + latitude + "," + longitude + "?o=json&jsonp=GeocodeCallback&key=" + BING_key;
 	CallRestService(url);
@@ -101,3 +118,4 @@ function GeocodeCallback(json) {
 }
 
 getCityAndCountry(data.location.latitude, data.location.longitude);
+*/
---
+++ phone_files/js/list_issues.js
@@ -0,0 +1,33 @@
+var getJSON = function(url) {
+	return new Promise(function(resolve, reject) {
+		var xhr = new XMLHttpRequest();
+		xhr.open('get', url, true);
+		xhr.responseType = 'json';
+		xhr.onload = function() {
+			var status = xhr.status;
+			if (status == 200) {
+				resolve(xhr.response);
+			} else {
+				reject(status);
+			}
+		};
+		xhr.send();
+	});
+};
+
+//*
+getJSON('http://debugworld.herokuapp.com/fetch_issues').then(function(data) {
+	html = "";
+	for (var i = 0; i < data.length; ++i) {
+		html += "<tr> \
+	                <td><a href=\"issue_details.html?id=" + data[i]._id + "\">" + data[i].name + "</a></td> \
+	                <td>" + data[i].dateCreated + "</td> \
+	                <td><span class=\"uk-text-success\">" + data[i].votes.up + "</span> / <span class=\"uk-text-danger\">" + data[i].votes.down + "</span></td> \
+	            </tr>";
+	}
+	document.getElementById("issuesList").innerHTML = html;
+	document.getElementById("nbIssues").innerText = data.length;
+}, function(status) {
+	alert('Something went wrong.');
+});
+//*/
